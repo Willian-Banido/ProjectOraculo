f6420222bd5d9f4337dd19c4a40c7fdb387e0e9e
Vinicius Andrade <112087469+Vinis-San@users.noreply.github.com>
2024-10-24 16:27:44 -0300

    V1 Pag inicial Front-End
    Tive auxilio do Copilot

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById('signIn').addEventListener('click', () => {
+    document.querySelector('.container').classList.remove('active');
+});
+
+document.getElementById('signUp').addEventListener('click', () => {
+    document.querySelector('.container').classList.add('active');
+});
